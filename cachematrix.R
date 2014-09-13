7d291de64e75190e74e738644a539919c38c7790
Rohi Fadlun <rohif@townflix.com>
2014-09-13 04:31:34 +0300

    better indentation

--- cachematrix.R
+++ cachematrix.R
@@ -6,17 +6,21 @@
 # 4. get the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-    inverse <- NULL
-    set <- function(y) {
-        x <<- y
-        inverse <<- NULL
-    }
-    get <- function() x
-    setinv <- function(solve) inverse <<- solve
-    getinv <- function() inverse
-    list(set = set, get = get,
-         setinv = setinv,
-         getinv = getinv)
+    
+        inverse <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        
+        get <- function() x
+        setinv <- function(solve) inverse <<- solve
+        getinv <- function() inverse
+        
+        list(set = set, get = get,
+                 setinv = setinv,
+                 getinv = getinv)
 }
 
 ## Return a matrix that is the inverse of 'x'
@@ -25,13 +29,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     
-    inverse <- x$getinv()
-    if (!is.null(inverse)) {
-        message("getting cached data")
-        return(inverse)
-    }
-    matrix <- x$get()
-    inverse <- solve(matrix, ...)
-    x$setinv(inverse)
-    inverse
+        inverse <- x$getinv()
+        
+        if (!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        
+        matrix <- x$get()
+        inverse <- solve(matrix, ...)
+        x$setinv(inverse)
+        inverse
+
 }
